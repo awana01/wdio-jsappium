fb91015ed2cda78826d3c0da3c678aa414aa6a82
raj001 <automationsingh.1983@gmail.com>
2022-10-09 18:34:53 +0530

    actions yml modify

--- wdio.conf.js
+++ wdio.conf.js
@@ -14,61 +14,30 @@ exports.config = {
         // 'path/to/excluded/files'
     ],
     
-    maxInstances: 10,
+    maxInstances: 1,
     capabilities: [{
 
-        maxInstances: 5,
+        //maxInstances: 1,
         browserName: 'chrome',
         acceptInsecureCerts: true
        
     }],
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
-    // logLevels: {
-    //     webdriver: 'info',
-    //     '@wdio/appium-service': 'info'
-    // },
-    //
-    // If you only want to run your tests until a specific amount of tests have failed use
-    // bail (default is 0 - don't bail, run all tests).
+    logLevel: 'error',
+    
     bail: 0,
     baseUrl: 'http://localhost',
-    // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
-    // Default request retries count
     connectionRetryCount: 3,
-    services: ['chromedriver','appium'],
+    services: ['chromedriver'],
     framework: 'mocha',
-    //
-    // The number of times to retry the entire specfile when it fails as a whole
-    // specFileRetries: 1,
-    //
-    // Delay in seconds between the spec file retry attempts
-    // specFileRetriesDelay: 0,
-    //
-    // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
-    // specFileRetriesDeferred: false,
-    //
-    // Test reporter for stdout.
-    // The only one supported by default is 'dot'
-    // see also: https://webdriver.io/docs/dot-reporter
     reporters: [['allure', {outputDir: 'allure-results'}]],
-
-
-    
-    //
-    // Options to be passed to Mocha.
-    // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 60000,
+        require: ['@babel/register']
     },
-    
-    // =====
-    // Hooks
-    // =====
     /**
      * Gets executed once before all workers get launched.
      * @param {Object} config wdio configuration object
